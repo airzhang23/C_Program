36f77c1b23cc5b37c68224164eca0073df564161
airzhang23 <air.zsl@gmail.com>
2015-07-29 17:19:46 +0800

    test

--- sample.c
+++ sample.c
@@ -5,6 +5,9 @@ int main (void)               /* a simple program to print number 1    */
    num = 1;                  /* assign a value to num        */
    printf("I am a simple "); /* use the printf() function    */
    printf("computer.\n");
+   for (int i=0;i<10;i++){
+	   printf("number %d\n",i);
+   }
    printf("My favorite number is %d because it is first.\n",num);
    return 0;
 }
